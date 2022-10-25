4ffccf7b9a102009532ef7d12630252b31d02ef3
Matthew Del Monte <mdel3089@gmail.com>
2022-10-24 21:14:10 -0700

    final project code

--- todo/src/App.js
+++ todo/src/App.js
@@ -8,21 +8,26 @@ function App() {
   const [currentTask, setCurrentTask] = useState("");
 
   const addTask = () => {
-    setTodolist([...todolist, currentTask])
+    setTodolist([...todolist, {task: currentTask, completed: false}])
     ref.current.value = "";
     setCurrentTask("");
-    console.log(todolist)
   }
 
   const deleteTask = (taskToDelete) => {
-    setTodolist(todolist.filter((item) => {return item !== taskToDelete}));
+    setTodolist(todolist.filter((item) => {return item.task !== taskToDelete}));
+  }
+
+  const completeTask = (taskToComplete) => {
+    setTodolist(todolist.map((task) => {
+      return task.task === taskToComplete ? {task: taskToComplete, completed: true} : {task: task.task, completed: task.completed ? true : false}
+    }));
   }
 
   return (
     <div className="App">
         <h1>TODO List</h1>
       <div>
-        <input ref={ref} type='text' placeholder='Task...' onChange={(event) => {setCurrentTask(event.target.value)}}/>
+        <input ref={ref} type='text' placeholder='Task...' onKeyDown={(event) => {if (event.keyCode === 13) addTask()}} onChange={(event) => {setCurrentTask(event.target.value)}}/>
         <button onClick={addTask}> Add Task </button>
       </div>
       <hr />
@@ -30,8 +35,9 @@ function App() {
         {todolist.map((task, key) => {
           return (
             <div id="task">
-              <li key={key}>{task}</li>
-              <button onClick={() => deleteTask(task)}> Remove </button>
+              <li className={task.completed && "complete"} key={key}>{task.task}</li>
+              <button onClick={() => completeTask(task.task)}> Complete </button>
+              <button onClick={() => deleteTask(task.task)}> Remove </button>
             </div>
           )
         })
