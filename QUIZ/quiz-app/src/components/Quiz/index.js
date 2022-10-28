7ee1f03c3391a4fa616036c5ff98964a58ded848
Matthew Del Monte <mdel3089@gmail.com>
2022-10-27 21:19:34 -0700

    Added questions

--- QUIZ/quiz-app/src/components/Quiz/index.js
+++ QUIZ/quiz-app/src/components/Quiz/index.js
@@ -1,14 +1,37 @@
 import "./style.css";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GameStateContext } from "../../utilities/Context";
+import { Questions } from "../../utilities/Questions";
 
 function Quiz() {
     // eslint-disable-next-line
     const { userName, setUserName } = useContext(GameStateContext);
+    const [curQuestion, setCurQuestion] = useState(0);
+    const [correct, setCorrect] = useState("");
+
+    const handleAnswer = (response) => {
+        if (Questions[curQuestion].answer === response) {
+            setCorrect("Correct!")
+        } else {
+            setCorrect("Sorry, incorrect.")
+        }
+    }
+
+    const handleNextQuestion = () => {
+        setCurQuestion(curQuestion + 1);
+        setCorrect("");
+    }
 
     return (
-        <div className={"menu"}>
-            <p>{userName}</p>
+        <div id={"quiz"} className={"menu"}>
+            <h2>Hi {userName}!</h2>
+            <h1>{Questions[curQuestion].prompt}</h1>
+            <button onClick={() => {handleAnswer("option1")}}>{Questions[curQuestion].option1}</button>
+            <button onClick={() => {handleAnswer("option2")}}>{Questions[curQuestion].option2}</button>
+            <button onClick={() => {handleAnswer("option3")}}>{Questions[curQuestion].option3}</button>
+            <button onClick={() => {handleAnswer("option4")}}>{Questions[curQuestion].option4}</button>
+            <h2>{correct}</h2>
+            <button onClick={() => {handleNextQuestion()}}>Next Question</button>
         </div>
     )
 }
---
+++ QUIZ/quiz-app/src/utilities/Questions.js
@@ -0,0 +1,19 @@
+
+export const Questions = [
+    {
+        prompt: "What State is Manhattan in?",
+        option1: "Nevada",
+        option2: "Georgia",
+        option3: "Florida",
+        option4: "New York",
+        answer: "option4"
+    },
+    {
+        prompt: "What is the Rust compiler called?",
+        option1: "Webpack",
+        option2: "gcc",
+        option3: "V8",
+        option4: "cargo",
+        answer: "option4"
+    }
+];
