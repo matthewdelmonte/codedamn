0c41c95e98dea0d839d604386a40c7f62c858e47
Matthew Del Monte <mdel3089@gmail.com>
2022-10-31 20:02:58 -0700

    final project code

--- NEWS/space-news/src/App.js
+++ NEWS/space-news/src/App.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [newsList, setNewsList] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.spaceflightnewsapi.net/v3/articles')
+    .then((res) => res.json())
+    .then((data) => { setNewsList(data); })
+  },[]);
+
+  console.log(newsList)
+
   return (
     <div className="App">
+      <div className="title">
       <h1>Space News</h1>
+      </div>
+      <div className="newsContainer">
+        {newsList.map((article, key) => {
+          return (
+          <div key={key} className="article" onClick={() => {window.location.href = article.url}}>
+            <h1>{article.title}</h1>
+            <img src={article.imageUrl} alt={"space_image"} ></img>
+            <article>{article.summary}</article>
+            <h4>{article.publishedAt}</h4>
+          </div>
+          )})
+        }
+      </div>
     </div>
   );
 }
